3fa1aa1c92f79f564ff3c0426fcbb5ef4c6977ec
Sebastian Prokesch <sebastian.prokesch@gmail.com>
2015-04-15 13:30:52 +0200

    fixed git complition

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -31,3 +31,15 @@ alias sl=ls # often screw this up
 
 alias afind='ack-grep -il'
 
+#Postgres aliases
+alias pg-up='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+alias pg-down='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+
+#Brew aliases
+alias brews='brew list -1'
+
+#Git aliases
+
+#
+
+# Tests
